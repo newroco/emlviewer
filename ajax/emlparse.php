39c7600337e02c9c0e280a289b42338f97ce2d53
Bogdan Incrosnatu <incrosnatubogdan@gmail.com>
2020-01-13 14:16:26 +0200

    Refs #3 added date

--- ajax/emlparse.php
+++ ajax/emlparse.php
@@ -13,6 +13,7 @@ if(isset($_POST['eml_file']) && !empty($_POST['eml_file'])){
 
 	$email .= '<p style="margin-top: 30px;">From: <strong>'.$message->getHeaderValue('From').'</strong></p>';
 	$email .= '<p>To: <strong>'.$message->getHeaderValue('To').'</strong></p>';
+	$email .= '<p>Date received: <strong>'.preg_replace('/\W\w+\s*(\W*)$/', '$1', $message->getHeaderValue('Date')).'</strong></p>';
 	if(!empty($message->getTextContent()))
 		$email .= '<button type="button" style="width: 150px;" id="toggle-text-content">Show content</button><div id="email-text-content" class="fade-out">Message:<br> '.$message->getTextContent().'</div>';
 	if(!empty($message->getHtmlContent()))
