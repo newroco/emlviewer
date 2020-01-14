604a9389f894ae7eaef3b58af42fe1e48f46286d
Bogdan Incrosnatu <incrosnatubogdan@gmail.com>
2020-01-14 13:37:38 +0200

    Refs #3 nginx routes post

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -6,6 +6,10 @@ use OCP\AppFramework\Http\TemplateResponse;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\Controller;
 
+use ZBateson\MailMimeParser\MailMimeParser;
+use ZBateson\MailMimeParser\Message;
+
+
 class PageController extends Controller {
 	private $userId;
 
@@ -24,8 +28,28 @@ class PageController extends Controller {
 	 * @NoAdminRequired
 	 * @NoCSRFRequired
 	 */
+	public function ParseEml(string $eml_file) {
+		$eml_file = $_POST['eml_file'];
+		if(isset($_POST['eml_file']) && !empty($_POST['eml_file'])){
+		$message = Message::from(urldecode($eml_file));
+		$email = '';
+
+		$email .= '<p style="margin-top: 30px;">From: <strong>'.$message->getHeaderValue('From').'</strong></p>';
+		$email .= '<p>To: <strong>'.$message->getHeaderValue('To').'</strong></p>';
+		$email .= '<p>Date received: <strong>'.preg_replace('/\W\w+\s*(\W*)$/', '$1', $message->getHeaderValue('Date')).'</strong></p>';
+		if(!empty($message->getTextContent()))
+			$email .= '<button type="button" style="width: 150px;" id="toggle-text-content">Show content</button><button type="button" style="width: 150px;" id="make-pdf">Download as PDF</button><div id="email-text-content" class="fade-out">Message:<br> '.$message->getTextContent().'</div>';
+		if(!empty($message->getHtmlContent()))
+			$email .= '<div style="flex: 1;">Content:<br> <iframe id="email-html-content" srcdoc="'.str_replace('"', '\'', $message->getHtmlContent()).'" style="width: 100%;min-height: 200px;height: 100%;"></iframe></div>';
+			// echo $email;
+			return $email;
+			exit;
+		} else {
+			echo 'Could not generate email preview';
+		}
+	}
+
 	public function index() {
 		return new TemplateResponse('emlviewer', 'index');  // templates/index.php
 	}
-
 }
