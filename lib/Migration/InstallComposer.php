406e85b0cbf49764bf814b7ec64957992134d810
Bogdan Incrosnatu <incrosnatubogdan@gmail.com>
2020-01-29 16:42:08 +0200

    Up

--- lib/Migration/InstallComposer.php
+++ lib/Migration/InstallComposer.php
@@ -2,7 +2,7 @@
 namespace OCA\EmlViewer\Migration;
 
 require dirname(__FILE__)."/../../vendor/autoload.php";
-
+use OCP\ILogger;
 use OCP\Migration\IOutput;
 use OCP\Migration\IRepairStep;
 
@@ -13,16 +13,23 @@ use Symfony\Component\Console\Input\ArrayInput;
 
 class InstallComposer implements IRepairStep {
 
-      public function __construct() {
+      public function __construct(ILogger $log) {
+        $this->log = $log;
       }
 
       /**
        * @param IOutput $output
        */
       public function run(IOutput $output) {
-        $input = new ArrayInput(array('command' => 'install -d '.dirname(__FILE__).'/../../'));
-        $application = new Application();
-        $application->setAutoExit(false); // prevent `$application->run` method from exitting the script
-        $application->run($input);
+        $this->log->debug("bunicamea");
+        try{
+          $input = new ArrayInput(array('command' => 'install -d='.dirname(__FILE__).'/../../'));
+          $application = new Application();
+          $application->setAutoExit(false); // prevent `$application->run` method from exitting the script
+          $application->run($input);
+        }catch(\Exception $e){
+          $this->log->debug($e->getMessage());
+        }
+        
       }
 }
