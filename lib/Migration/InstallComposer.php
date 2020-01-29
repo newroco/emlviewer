bf977360755f4d7bb7510b411175d950cd019756
Bogdan Incrosnatu <incrosnatubogdan@gmail.com>
2020-01-29 14:13:00 +0200

    install composer

--- lib/Migration/InstallComposer.php
+++ lib/Migration/InstallComposer.php
@@ -5,7 +5,6 @@ require dirname(__FILE__)."/../../vendor/autoload.php";
 
 use OCP\Migration\IOutput;
 use OCP\Migration\IRepairStep;
-use OCP\ILogger;
 
 use Composer\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
@@ -14,11 +13,7 @@ putenv('COMPOSER_HOME=' . __DIR__ . '/vendor/bin/composer');
 
 class InstallComposer implements IRepairStep {
 
-      /** @var ILogger */
-      protected $logger;
-
-      public function __construct(ILogger $logger) {
-              $this->logger = $logger;
+      public function __construct() {
       }
 
       /**
@@ -29,7 +24,5 @@ class InstallComposer implements IRepairStep {
         $application = new Application();
         $application->setAutoExit(false); // prevent `$application->run` method from exitting the script
         $application->run($input);
-        $this->logger->warning("Composer was installed!", ["app" => "EmlViewer"]);
-
       }
 }
